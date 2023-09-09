36ddcdbfb3de766bb2d1fa21f92c9ff14fcd5c24
Perfect Imperfection <63294758+nvth@users.noreply.github.com>
2023-09-09 12:02:04 +0700

    Update create-vbs.ps1

--- create-vbs.ps1
+++ create-vbs.ps1
@@ -3,4 +3,3 @@ if (Test-Path BurpSuiteProfessional.vbs) {
 echo "Set WshShell = CreateObject(`"WScript.Shell`")" > BurpSuiteProfessional.vbs
 add-content BurpSuiteProfessional.vbs "WshShell.Run chr(34) & `"$pwd\Burp.bat`" & Chr(34), 0"
 add-content BurpSuiteProfessional.vbs "Set WshShell = Nothing"
-echo "Burp-Suite-Pro.vbs file is created.`n"
